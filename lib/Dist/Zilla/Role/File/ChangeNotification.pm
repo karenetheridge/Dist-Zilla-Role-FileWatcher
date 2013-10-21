ef179c99d00dbab8160cf93ba8ec874fd46500f0
Karen Etheridge <ether@cpan.org>
2013-10-20 18:44:52 -0700

    initial commit
    via [Git::Init] in Dist-Zilla-PluginBundle-Author-ETHER-0.032, github profile.

---
+++ lib/Dist/Zilla/Role/File/ChangeNotification.pm
@@ -0,0 +1,54 @@
+use strict;
+use warnings;
+package Dist::Zilla::Role::File::ChangeNotification;
+# ABSTRACT: ...
+# vim: set ts=8 sw=4 tw=78 et :
+
+
+
+1;
+__END__
+
+=pod
+
+=head1 SYNOPSIS
+
+    use Dist::Zilla::Role::File::ChangeNotification;
+
+    ...
+
+=head1 DESCRIPTION
+
+...
+
+=head1 FUNCTIONS/METHODS
+
+=over 4
+
+=item * C<foo>
+
+...
+
+=back
+
+=head1 SUPPORT
+
+=for stopwords irc
+
+Bugs may be submitted through L<the RT bug tracker|https://rt.cpan.org/Public/Dist/Display.html?Name=Dist-Zilla-Role-File-ChangeNotification>
+(or L<bug-Dist-Zilla-Role-File-ChangeNotification@rt.cpan.org|mailto:bug-Dist-Zilla-Role-File-ChangeNotification@rt.cpan.org>).
+I am also usually active on irc, as 'ether' at C<irc.perl.org>.
+
+=head1 ACKNOWLEDGEMENTS
+
+...
+
+=head1 SEE ALSO
+
+=begin :list
+
+* L<foo>
+
+=end :list
+
+=cut
