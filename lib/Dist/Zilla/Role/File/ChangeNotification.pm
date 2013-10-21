40f033e83bc365b955cc91b9f7a9b03598a54f6e
Karen Etheridge <ether@cpan.org>
2013-10-20 18:47:50 -0700

    initial code, tests, docs

--- lib/Dist/Zilla/Role/File/ChangeNotification.pm
+++ lib/Dist/Zilla/Role/File/ChangeNotification.pm
@@ -1,10 +1,62 @@
 use strict;
 use warnings;
 package Dist::Zilla::Role::File::ChangeNotification;
-# ABSTRACT: ...
+# ABSTRACT: Receive notification when something changes a file's contents
 # vim: set ts=8 sw=4 tw=78 et :
 
+use Moose::Role;
+use Digest::MD5 'md5_hex';
+use Encode 'encode_utf8';
+use namespace::autoclean;
 
+has _content_checksum => ( is => 'rw', isa => 'Str' );
+
+has on_changed => (
+    is => 'rw',
+    isa => 'CodeRef',
+    traits => ['Code'],
+    handles => { has_changed => 'execute_method' },
+    lazy => 1,
+    default => sub {
+        sub { die 'content of ', shift->name, ' has changed!' }
+    },
+);
+
+sub watch_file
+{
+    my $self = shift;
+
+    return if $self->_content_checksum;
+
+    # this may not be the correct encoding, but things should work out okay
+    # anyway - all we care about is deterministically getting bytes back
+    $self->_content_checksum($self->__calculate_checksum);
+    return;
+}
+
+sub __calculate_checksum
+{
+    my $self = shift;
+    md5_hex(encode_utf8($self->content))
+}
+
+around content => sub {
+    my $orig = shift;
+    my $self = shift;
+
+    # pass through if getter
+    return $self->$orig if @_ < 1;
+
+    my $content = shift;
+    $self->$orig($content);
+
+    # do nothing extra if we haven't got a checksum yet
+    my $old_checksum = $self->_content_checksum;
+    return $content if not $old_checksum;
+
+    $self->has_changed($content) if $self->__calculate_checksum ne $old_checksum;
+    return $content;
+};
 
 1;
 __END__
@@ -13,21 +65,50 @@ __END__
 
 =head1 SYNOPSIS
 
-    use Dist::Zilla::Role::File::ChangeNotification;
+    package Dist::Zilla::Plugin::MyPlugin;
+    sub some_phase
+    {
+        my $self = shift;
 
-    ...
+        my ($source_file) = grep { $_->name eq $self->source } @{$self->zilla->files};
+        # ... do something with this file ...
+
+        Dist::Zilla::Role::File::ChangeNotification->meta->apply($source_file);
+        my $plugin = $self;
+        $file->on_changed(sub {
+            $plugin->log_fatal('someone tried to munge ', shift->name,
+                ' after we read from it. You need to adjust the load order of your plugins.');
+        });
+        $file->watch_file;
+    }
 
 =head1 DESCRIPTION
 
-...
+This is a role for L<Dist::Zilla::Role::File> objects which gives you a
+mechanism for detecting and acting on files changing their content. This is
+useful if your plugin performs an action based on a file's content (perhaps
+copying that content to another file), and then later in the build process,
+that source file's content is later modified.
 
-=head1 FUNCTIONS/METHODS
+=head1 ATTRIBUTES
 
 =over 4
 
-=item * C<foo>
+=item * C<on_changed>: a sub which is invoked against the file when the file's
+content has changed.  The new file content is passed as an argument.  If you
+need to do something in your plugin at this point, define the sub as a closure
+over your plugin object, as demonstrated in the L</SYNOPSIS>.
 
-...
+=back
+
+=head1 METHODS
+
+=over 4
+
+=item * C<watch_file> - Once this method is called, every subsequent change to
+the file's content will result in your C<on_changed> sub being invoked against
+the file.  The new content is passed as the argument to the sub; The return
+value is ignored.
 
 =back
 
@@ -39,15 +120,12 @@ Bugs may be submitted through L<the RT bug tracker|https://rt.cpan.org/Public/Di
 (or L<bug-Dist-Zilla-Role-File-ChangeNotification@rt.cpan.org|mailto:bug-Dist-Zilla-Role-File-ChangeNotification@rt.cpan.org>).
 I am also usually active on irc, as 'ether' at C<irc.perl.org>.
 
-=head1 ACKNOWLEDGEMENTS
-
-...
-
 =head1 SEE ALSO
 
 =begin :list
 
-* L<foo>
+* L<Dist::Zilla::File::OnDisk>
+* L<Dist::Zilla::File::InMemory>
 
 =end :list
 
--- t/01-basic.t
+++ t/01-basic.t
@@ -3,8 +3,76 @@ use warnings FATAL => 'all';
 
 use Test::More;
 use if $ENV{AUTHOR_TESTING}, 'Test::Warnings';
+use Test::Fatal;
+use Test::DZil;
+use Path::Tiny;
+
+# we chdir before attempting to load the module, so we need to load it now or
+# our relative path in @INC will be for naught.
 use Dist::Zilla::Role::File::ChangeNotification;
 
-fail('this test is TODO!');
+{
+    package Dist::Zilla::Plugin::MyPlugin;
+    use Moose;
+    use Module::Runtime 'use_module';
+    use Moose::Util::TypeConstraints;
+    with 'Dist::Zilla::Role::FileMunger';
+    has source_file => (
+        is => 'ro', isa => 'Str',
+        required => 1,
+    );
+    has function => (
+        is => 'ro', isa => enum([qw(uc lc)]),
+        required => 1,
+    );
+    sub munge_files
+    {
+        my $self = shift;
+
+        my ($file) = grep { $_->name eq $self->source_file } @{$self->zilla->files};
+
+        # upper-case all the comments
+        my $content = $file->content;
+        $content =~ s/^# (.+)$/'# ' . uc($1)/me if $self->function eq 'uc';
+        $content =~ s/^# (.+)$/'# ' . lc($1)/me if $self->function eq 'lc';
+        $file->content($content);
+
+        # lock the file so no one can alter it after we have touched it
+
+        use_module('Dist::Zilla::Role::File::ChangeNotification')->meta->apply($file);
+        my $plugin = $self;
+        $file->on_changed(sub {
+            my $self = shift;
+            $plugin->log_fatal('someone tried to munge ' . $self->name
+                .' after we read from it. You need to adjust the load order of your plugins.');
+        });
+
+        $file->watch_file;
+    }
+}
+
+my $tzil = Builder->from_config(
+    { dist_root => 't/does-not-exist' },
+    {
+        add_files => {
+            'source/dist.ini' => simple_ini(
+                [ GatherDir => ],
+                [ MyPlugin => uc => { function => 'uc', source_file => 'lib/Foo.pm' } ],
+                [ MyPlugin => lc => { function => 'lc', source_file => 'lib/Foo.pm' } ],
+            ),
+            path(qw(source lib Foo.pm)) => <<CODE,
+package Foo;
+# hErE IS a coMMent!
+1
+CODE
+        },
+    },
+);
+
+like(
+    exception { $tzil->build },
+    qr{someone tried to munge lib/Foo.pm after we read from it. You need to adjust the load order of your plugins},
+    'detected attempt to change README after signature was created from it',
+);
 
 done_testing;
