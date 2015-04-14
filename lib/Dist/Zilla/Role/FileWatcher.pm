37098ce23b6ebb0e56583ce0a7eacf4f8c923eb5
Karen Etheridge <ether@cpan.org>
2015-04-14 15:09:18 -0700

    fix vim modeline

--- lib/Dist/Zilla/Role/FileWatcher.pm
+++ lib/Dist/Zilla/Role/FileWatcher.pm
@@ -2,7 +2,7 @@ use strict;
 use warnings;
 package Dist::Zilla::Role::FileWatcher;
 # ABSTRACT: Receive notification when something changes a file's contents
-# vim: set ts=8 sw=4 tw=78 et :
+# vim: set ts=8 sts=4 sw=4 tw=78 et :
 
 our $VERSION = '0.006';
 
